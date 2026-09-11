849fc83e9d91a98389d69f3522add44eb52443f4
Classy Dev <dev@classy.com>
2026-09-11 18:26:41 +0530

    Update Supabase URL and key for malka project

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://voflbggkzldfwmedxcxk.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZmxiZ2dremxkZndtZWR4Y3hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwMTM5NTYsImV4cCI6MjEwNDU4OTk1Nn0.kqsENN7CiASKl6CBtibc0TQiWApmgx5sVu-B17baLlw";
+const supabaseUrl = "https://ksfnhngkxzogprpgqlrd.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZm5obmdreHpvZ3BycGdxbHJkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNzgxNjksImV4cCI6MjEwNDg1NDE2OX0.AEjTc3bEKeRkI6aHU6Dy1o1Kp9PC21EBJwkgCrflcZs";
 
 const originalConsoleError = console.error;
 const originalConsoleWarn = console.warn;
